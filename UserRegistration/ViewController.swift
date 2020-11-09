23467ecd7767c3da7b1719e7f68c79f87a2c293e
SergeyGolenko <1989sergiotay@gmail.com>
2020-11-09 12:03:54 +0200

    Initial Commit

---
+++ UserRegistration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserRegistration
+//
+//  Created by Сергей Голенко on 09.11.2020.
+//  Copyright © 2020 Сергей Голенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
